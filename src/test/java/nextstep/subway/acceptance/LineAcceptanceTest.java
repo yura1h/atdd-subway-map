d775ebfa7b7be9d8290e4687e7c0b0dd80aed02d
H-YURA <61656687+H-YURA@users.noreply.github.com>
2022-01-25 16:32:24 +0900

    노선목록조회

--- src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/LineAcceptanceTest.java
@@ -1,15 +1,19 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import net.minidev.json.parser.JSONParser;
 import org.assertj.core.api.Assertions;
+import org.json.JSONArray;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.util.HashMap;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -48,6 +52,42 @@ class LineAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철 노선 목록 조회")
     @Test
     void getLines() {
+
+        // given
+        String 일호선 = "1호선";
+        String 이호선 = "2호선";
+        HashMap <Object, Object> params1 = new HashMap <>();
+        params1.put("name", 일호선);
+        params1.put("color", "blue");
+
+        ExtractableResponse<Response> response1 = RestAssured
+                .given().log().all()
+                .body(params1).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all().extract();
+
+        HashMap <Object, Object> params2 = new HashMap <>();
+        params2.put("name", 이호선);
+        params2.put("color", "green");
+        ExtractableResponse<Response> response2 = RestAssured
+                .given().log().all()
+                .body(params2).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/lines")
+                .then().log().all().extract();
+
+        // when
+
+        ExtractableResponse<Response> response3 = RestAssured
+                .when()
+                .get("/lines")
+                .then().log().all().extract();
+
+        Assertions.assertThat(response3.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        List <Object> res = response3.jsonPath().getList("name");
+        assertThat(res).contains(일호선, 이호선);
     }
 
     /**
